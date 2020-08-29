55439a64afb5b35a6c87793a92b30510304af3c3
watcharapol yotadee <watcharapolyotadee@outlook.com>
2020-08-29 13:37:33 +0700

    For or

--- Sum_square_difference_Week3/Source.cpp
+++ Sum_square_difference_Week3/Source.cpp
@@ -5,10 +5,10 @@ int main()
 	int num1[5];
 	num1[0] = 1;
 	num1[1] = num1[2] = num1[3] = num1[4] = 0;
-	for (num1[0] = 1; num1[0] < 100; num[0]++)
+	for (num1[0] = 1; num1[0] < 100; num1[0]++)
 	{
-
-
+		num1[1] += (num1[0] * num1[0]);
+		num1[2] += num1[0];
 	}
 
 
