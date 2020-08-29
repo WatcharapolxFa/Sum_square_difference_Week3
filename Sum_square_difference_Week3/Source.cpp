c645dc2b62671e8a9a130fc2d4fd194252bd6084
watcharapol yotadee <watcharapolyotadee@outlook.com>
2020-08-29 13:54:26 +0700

    85 %

--- Sum_square_difference_Week3/Source.cpp
+++ Sum_square_difference_Week3/Source.cpp
@@ -1,16 +1,28 @@
 #include<stdio.h>
 
+
+
+
+int Sum(int sum_squar, int sum_num)
+{
+	int avg = 0;
+	sum_squar = sum_squar * sum_squar;
+	avg = sum_squar - sum_num;
+
+return avg;
+}
 int main()
 {
-	int num1[5];
-	num1[0] = 1;
-	num1[1] = num1[2] = num1[3] = num1[4] = 0;
-	for (num1[0] = 1; num1[0] < 100; num1[0]++)
+	int num[4];
+	num[0] = 1; // x
+	num[1] = num[2] = num[3] = 0;
+	for (num[0] = 1; num[0] < 100; num[0]++)
 	{
-		num1[1] += (num1[0] * num1[0]);
-		num1[2] += num1[0];
+		num[1] += (num[0] * num[0]);
+		num[2] += num[0];
 	}
-
+	num[3] = Sum(num[1] , num[2]);
+	printf_s("%d", num[3]);
 
 	return 0;
 }
